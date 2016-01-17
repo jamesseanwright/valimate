7d0e589dc28016c2b87b4119be4c4f80776d2cc3
James Wright <james@jamesswright.co.uk>
2016-01-17 13:39:43 +0000

    Refactor HTTP service

--- code/services/httpService.js
+++ code/services/httpService.js
@@ -4,8 +4,34 @@ const http = require('http');
 const https = require('https');
 const urlParser = require('./urlParser');
 
-function makeRequest(method, url, options) {
-	return new Promise((resolve, reject) => {
+module.exports = {
+	post(url, options) {
+		return this._makeRequest('POST', url, options);
+	},
+
+	get(url, options) {
+		return this._makeRequest('GET', url, options);
+	},
+
+	_makeRequest(method, url, options) {
+		return new Promise((resolve, reject) => {
+			const requestOptions = this._createOptions(method, url, options);
+
+			const requester = this._getRequester(url);
+
+			const request = requester(requestOptions, res => this._onResponse(res, resolve, reject));
+
+			request.on('error', reject);
+
+			if (requestOptions.body) {
+				request.write(requestOptions.body);
+			}
+
+			request.end();
+		});
+	},
+
+	_createOptions(method, url, options) {
 		var requestOptions = {
 			hostname: urlParser.getHost(url),
 			path: urlParser.getPath(url),
@@ -16,42 +42,22 @@ function makeRequest(method, url, options) {
 			requestOptions = Object.assign(requestOptions, options);
 		}
 
-		const requester = getRequester(url);
-
-		const request = requester(requestOptions, res => onResponse(res, resolve, reject));
-
-		request.on('error', reject);
-
-		if (requestOptions.body) {
-			request.write(requestOptions.body);
-		}
-
-		request.end();	
-	});
-}
-
-function getRequester(url) {
-	return urlParser.isHttps(url) ? https.request : http.request;
-}
-
-function onResponse(response, resolve, reject) {
-	const hasResponseFailed = response.status >= 400;
-	var responseBody = '';
-
-	if (hasResponseFailed) {
-		reject(`Request to ${response.url} failed with HTTP ${response.status}`);
-	}
-
-	response.on('data', chunk => responseBody += chunk.toString());
-	response.on('end', () => resolve(responseBody));
-}
-
-module.exports = {
-	post(url, options) {
-		return makeRequest('POST', url, options);
+		return requestOptions
 	},
 
-	get(url, options) {
-		return makeRequest('GET', url, options);
+	_onResponse(response, resolve, reject) {
+		const hasResponseFailed = response.status >= 400;
+		var responseBody = '';
+
+		if (hasResponseFailed) {
+			reject(`Request to ${response.url} failed with HTTP ${response.status}`);
+		}
+
+		response.on('data', chunk => responseBody += chunk.toString());
+		response.on('end', () => resolve(responseBody));
+	},
+
+	_getRequester(url) {
+		return urlParser.isHttps(url) ? https.request : http.request;
 	}
 };
